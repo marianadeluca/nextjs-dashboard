9ed17ee3bf7f189571f0b1162cf983a81e5c26f8
MarianaReis <mariana.reis@radixeng.com>
2025-09-29 08:58:49 -0300

    commit unsaved files

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,7 +7,7 @@ import { Suspense } from 'react';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Invoices',
+  title: 'Dashboard',
 };
  
 export default async function Page() {
